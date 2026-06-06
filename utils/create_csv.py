ad239450702991bc14d5242a9315f38805994e4a
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-06 06:27:18 -0400

    update

--- utils/create_csv.py
+++ utils/create_csv.py
@@ -54,6 +54,11 @@ def save_data():
         print("[red]❌ file not found[/red]")
 
 def load_data():
+
+    User.all_users.clear()
+    Project.all_projects.clear()
+    Task.all_tasks.clear()
+
     try:
             
         with open(f"data/user_data.csv", "r") as file:
