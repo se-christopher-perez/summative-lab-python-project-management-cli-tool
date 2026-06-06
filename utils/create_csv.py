2abcb800c24e7c3d0a7efd672380fed5d25e6475
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-06 04:00:41 -0400

    Update: added create_csv code and tested to created user_data

---
+++ utils/create_csv.py
@@ -0,0 +1,29 @@
+import csv
+from models import User, Project, Task
+
+if __name__ == "__main__":
+
+    dude = User.create_new_user("Dude", "dude@email.com")
+
+    cli = Project.create_new_project("cli", "Learn Python", "6/5/2026")
+
+    task = Task.create_new_task("Create cli", "Work In Progress", "6/20/2026")
+
+    filename = "user_data.csv"
+
+    with open(f"data/{filename}", "w", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["ID", "Name", "Email"])
+        for user in User.all_users:
+            writer.writerow([user.id, user.name, user.email])
+
+        writer.writerow(["ID", "Title", "Description", "Due_date"])
+        for project in Project.all_projects:
+            writer.writerow([cli.id, cli.title, cli.description, cli.due_date])
+
+        writer.writerow(["ID", "Title", "Status", "Assigned_to"])
+        for task in Task.all_tasks:
+            writer.writerow([task.id, task.title, task.status, task.assigned_to])
+
+    print(f"✅ {filename} Saved")
